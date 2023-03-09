5c1a2905fe7fc87fbf030a71fb16c7f8f52adf20
blackedder0815 <dev@patson.de>
2023-03-09 12:23:11 +0100

    Removed obsolet code

--- main.js
+++ main.js
@@ -26,9 +26,6 @@ class MytkstarGps extends utils.Adapter {
 			name: "mytkstar-gps",
 		});
 		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
 
 		this.queryProcessID = null;
@@ -41,7 +38,7 @@ class MytkstarGps extends utils.Adapter {
 	async updateStates(context) {
 		try{
 			context.log.debug("intervall triggered");
-			
+
 			// fetch login-properties
 			const responseInit = await axios.get("https://www.mytkstar.net/Login.aspx");
 			const cookies = responseInit.headers["set-cookie"];
@@ -171,67 +168,12 @@ class MytkstarGps extends utils.Adapter {
 			if(this.queryProcessID) {
 				this.clearInterval(this.queryProcessID);
 			}
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
 
 			callback();
 		} catch (e) {
 			callback();
 		}
 	}
-
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	// /**
-	//  * Is called if a subscribed state changes
-	//  * @param {string} id
-	//  * @param {ioBroker.State | null | undefined} state
-	//  */
-	// onStateChange(id, state) {
-	// 	if (state) {
-	// 		// The state was changed
-	// 		this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-	// 	} else {
-	// 		// The state was deleted
-	// 		this.log.info(`state ${id} deleted`);
-	// 	}
-	// }
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
-
 }
 
 if (require.main !== module) {
